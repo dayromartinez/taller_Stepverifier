fff6f22c200b2f511daa192e921ff956a83cb18b
Dayro Martinez <djmd96@hotmail.com>
2021-12-12 19:36:44 -0500

    Pruebas básicas StepVerfier OK

---
+++ Spring_Webflux_Test/src/main/java/com/StepVerifier/Spring_Webflux_Test/SpringWebfluxTestApplication.java
@@ -0,0 +1,13 @@
+package com.StepVerifier.Spring_Webflux_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxTestApplication.class, args);
+	}
+
+}
